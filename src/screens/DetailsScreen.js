aac02a1892732f608604d7adf3703ee4d34999bc
YahirDegante <105467759+YahirDegante@users.noreply.github.com>
2023-03-24 15:19:06 -0600

    Add files via upload

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,25 +1,32 @@
 import { StyleSheet, Text, View, Button } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function DetailsScreen(props) {
   const { navigation } = props;
+  const [data, setData]=useState([]);
+  const getPlaces = async () => {
+    try {
+      const response = await fetch(
+        "http://192.168.62.185:3000/travel/api/place",
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      const json = await response.json();
+      console.log(json);
+      setData(json);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  useEffect(()=>{
+    getPlaces()
+  },[])
   return (
     <View>
-      <Text>DetailsScreen</Text>
-      <Button
-        title="home"
-        onPress={() => {
-          navigation.navigate("index");
-        }}
-      />
-
-      <Button
-        title="Infor"
-        onPress={() => {
-          navigation.navigate("info");
-        }}
-      />
+      <Text>Aqui van los lugares</Text>
     </View>
   );
 }
