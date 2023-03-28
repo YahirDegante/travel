0ebb31728021ec9c642c7821e6da7ebc5f8d1543
YahirDegante <105467759+YahirDegante@users.noreply.github.com>
2023-03-28 12:43:54 -0600

    Add files via upload

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,12 +1,24 @@
-import { StyleSheet, Text, View, Button, ImageBackground, Image } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ImageBackground,
+  Image,
+} from "react-native";
 import React, { useEffect, useState, useRef } from "react";
 import Carousel from "react-native-snap-carousel";
+import { Rating, Icon } from "react-native-elements";
+import Modal from "../components/common/Modal";
 
 export default function DetailsScreen(props) {
   const { navigation } = props;
   const [data, setData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [renderComponent, setRenderComponent] = useState(null);
   const carousel = useRef();
   const [activeIndex, setActiveIndex] = useState(0);
+  const onClose = () => setShowModal((prevState) => !prevState);
   const getPlaces = async () => {
     try {
       const response = await fetch(
@@ -28,17 +40,50 @@ export default function DetailsScreen(props) {
     getPlaces();
   }, []);
 
-  const renderItem = ({index, item})=>{
-    return(
-      <View style={styles.card}>
-        <Image
-        style={styles.img}
-        source={{uri: item.image}} 
-        />
-        <Text>{item.name}</Text>
-      </View>
-    )
+  const playVideo =(video)=>{
+    // console.log("reproducir Video...")
+    // console.log(video)
+    setRenderComponent(<Text>Aqui se ve el video</Text>)
+    onClose()
   }
+  const showLocation=()=>{
+    console.log("Mostrando locacion...")
+  }
+
+  const renderItem = ({ index, item }) => {
+    return (
+      <View style={styles.card}>
+        <Image style={styles.img} source={{ uri: item.image }} />
+        <View style={styles.icons} >
+          <Icon
+          type="material-community"
+          name="youtube"
+          color="red"
+          size={40}
+          onPress={()=>playVideo(item.video)}
+          />
+          <Icon
+          type="material-community"
+          name="google-maps"
+          color="green"
+          size={40}
+          onPress={showLocation}
+          />
+        </View>
+        <Text style={styles.name}>{item.name}</Text>
+        <Text style={styles.description}>{item.description}</Text>
+        <Rating
+          type="star"
+          startingValue={parseFloat(item.rating)}
+          fraction={1}
+          imageSize={30}
+          style={styles.rating}
+          readonly={true}
+        />
+        <Text style={styles.points}>Calificación: {item.rating}/5</Text>
+      </View>
+    );
+  };
 
   return (
     <View style={styles.container}>
@@ -57,6 +102,9 @@ export default function DetailsScreen(props) {
           renderItem={renderItem}
         />
       </ImageBackground>
+      {renderComponent &&(
+        <Modal visible={showModal} close={onClose}>{renderComponent}</Modal>
+      )}
     </View>
   );
 }
@@ -67,26 +115,46 @@ const styles = StyleSheet.create({
   },
   background: {
     flex: 1,
-    width:"100%",
+    width: "100%",
     height: "100%",
     alignItems: "center",
     justifyContent: "center",
   },
-  card:{
-    backgroundColor:"#fff",
-    borderRadius:10,
-    height:"80%",
-    padding:40,
-    marginTop:50,
-    marginHorizontal:25,
-    alignItems:"center",
-    justifyContent:"center",
-    borderWidth:1.5,
-    borderColor: "#0D5BD7"
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    height: "80%",
+    padding: 40,
+    marginTop: 50,
+    marginHorizontal: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1.5,
+    borderColor: "#0D5BD7",
   },
-  img:{
+  img: {
     height: "50%",
-    width:"90%",
+    width: "90%",
     borderRadius: 5,
+  },
+  name: {
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  description: {
+    fontSize: 16,
+  },
+  fontSizerating: {
+    paddingVertical: 6,
+  },
+  points: {
+    fontSize:14,
+    fontWeight:"bold"
+  },
+  icons:{
+    flexDirection:"row",
+    justifyContent:"space-between",
+    width: "45%",
+
   }
 });
